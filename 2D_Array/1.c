e0466eaa76da736d01e51d4dd63f48cd7d0ed504
Salman <kans6643@gmail.com>
2025-04-20 05:35:26 +0000

    Add additional test print statements to main function

--- 2D_Array/1.c
+++ 2D_Array/1.c
@@ -4,4 +4,6 @@ int main()
 {
     printf("Hello World\n");
     printf("This is to test the github commit function :\n");
+    printf("This is to test the github commit function 2nd time::\n");
+    printf("This is to test the github commit function 3rd time::\n");
 }
