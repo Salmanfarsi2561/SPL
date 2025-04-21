502cb4cd72a42786bc50c378cdb1596a225ecd13
Salman <kans6643@gmail.com>
2025-04-21 15:53:00 +0000

    Printing Average of n input

--- Loop/4.c
+++ Loop/4.c
@@ -1,25 +1,20 @@
 #include<stdio.h>
 
-
 int main(){
-
     int n;
     printf("Enter a number: ");
     scanf("%d", &n);
     
+    int a;
+    int sum=0;
     for(int i = 1; i <= n; i++){
-        
-        if(i%2==0){
-            printf("0");
-        }
-        else printf("1");
-        
-        if(i<n){
-            printf(", ");
-        }
-        
-        
+        printf("Enter number %d: ", i);
+        scanf("%d",&a);
+        sum+=a;
     }
+
+    int avg = sum/n;
+    printf("Average: %d\n", avg);
     printf("\n");
     return 0;
 }
