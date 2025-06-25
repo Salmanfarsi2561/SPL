f0862dcff721a4562873d5fb8d02b216437278f0
Salman <kans6643@gmail.com>
2025-06-25 15:57:12 +0000

    7Updated

--- 1D_Array/7.c
+++ 1D_Array/7.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    
+    int start = 0, end = n - 1, temp;
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    printf("Reversed array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
 
